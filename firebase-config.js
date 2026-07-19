3aa93d06ff1b0f3fa449ed8b00e806e55c2e81a9
Kingachu-alt <koffianmusic@gmail.com>
2026-07-19 13:10:43 +0100

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,12 @@
 // ============================================================
 
 const FIREBASE_CONFIG = {
-  apiKey:            "PASTE_YOUR_apiKey_HERE",
-  authDomain:        "PASTE_YOUR_authDomain_HERE",
-  projectId:         "PASTE_YOUR_projectId_HERE",
-  storageBucket:     "PASTE_YOUR_storageBucket_HERE",
-  messagingSenderId: "PASTE_YOUR_messagingSenderId_HERE",
-  appId:             "PASTE_YOUR_appId_HERE"
+  apiKey:            "AIzaSyAxPa1fcpadkOneJ-LN8yzcsaArZrzwVaA",
+  authDomain:        "nova-e787f.firebaseapp.com",
+  projectId:         "nova-e787f",
+  storageBucket:     "nova-e787f.firebasestorage.app",
+  messagingSenderId: "213786701565",
+  appId:             "1:213786701565:web:ff49c683248a9cd8155894"
 };
 
 // Admin password for the admin panel
